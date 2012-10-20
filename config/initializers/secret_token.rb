b98b542de39d066aec7243d068b4078c3b75b450
Kim Godard <kgodard@comverge.com>
2012-10-20 10:03:22 -0600

    rails_apps_composer: initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Gigblender::Application.config.secret_token = 'a4a06f912384aa35827e229f11e9a5ee6b0d152ac52bbb54557eaae5ff16d74c07569b5e76729e44b3c1694d9e461c61fef05e02fc46d5f20befb5ccd40ccdba'
