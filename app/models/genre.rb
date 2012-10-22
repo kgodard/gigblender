9a9d5c7a8e4330954971f11ee9ffbea4aa63bdbb
Kim Godard <kgodard@comverge.com>
2012-10-22 08:23:27 -0600

    Add Genre model and tables

---
+++ app/models/genre.rb
@@ -0,0 +1,17 @@
+class Genre < ActiveRecord::Base
+  attr_accessible :name
+
+  has_and_belongs_to_many :artists
+  # has_and_belongs_to_many :slots
+
+  def self.all_genre_names
+    all.map(&:name)
+  end
+
+  def self.seed_data!
+    self.delete_all
+
+    I18n.translate("faker.genre.name").each { |g| Genre.create(:name => g) }
+  end
+end
+
