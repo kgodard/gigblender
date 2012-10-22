5493937abad56b40e809a933d2eb83c27e83c09d
Kim Godard <kgodard@comverge.com>
2012-10-22 08:22:41 -0600

    Add genre associations and image uploader to Artist

--- app/models/artist.rb
+++ app/models/artist.rb
@@ -1,5 +1,13 @@
 class Artist < ActiveRecord::Base
   attr_accessible :available_friday, :available_monday, :available_saturday, :available_sunday,
     :available_thursday, :available_tuesday, :available_wednesday, :covers_percentage,
-    :description, :image, :name, :phone, :user_id, :website, :zipcode_id
+    :description, :image, :name, :phone, :user_id, :website, :zipcode_id, :genre_ids
+
+  validates :name, :presence => true
+
+  belongs_to :user
+
+  has_and_belongs_to_many :genres
+
+  mount_uploader :image, ImageUploader
 end
