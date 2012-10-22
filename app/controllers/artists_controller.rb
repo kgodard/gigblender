e33d1d7261adcee69d5e81873d99de6fc03cfd4f
Kim Godard <kgodard@comverge.com>
2012-10-22 08:20:52 -0600

    Convert ArtistController to InheritedResources controller

--- app/controllers/artists_controller.rb
+++ app/controllers/artists_controller.rb
@@ -1,83 +1,18 @@
-class ArtistsController < ApplicationController
-  # GET /artists
-  # GET /artists.json
-  def index
-    @artists = Artist.all
+class ArtistsController < InheritedResources::Base
+  belongs_to :user, :optional => true
 
-    respond_to do |format|
-      format.html # index.html.erb
-      format.json { render json: @artists }
-    end
-  end
+  # has_scope :in_genres, :type => :array
+  # has_scope :limit, :default => 10
+  # has_scope :in_state
+  # has_scope :on_date
 
-  # GET /artists/1
-  # GET /artists/1.json
-  def show
-    @artist = Artist.find(params[:id])
+  # def index
+  #   debugger
+  # end
 
-    respond_to do |format|
-      format.html # show.html.erb
-      format.json { render json: @artist }
-    end
-  end
-
-  # GET /artists/new
-  # GET /artists/new.json
-  def new
-    @artist = Artist.new
-
-    respond_to do |format|
-      format.html # new.html.erb
-      format.json { render json: @artist }
-    end
-  end
-
-  # GET /artists/1/edit
-  def edit
-    @artist = Artist.find(params[:id])
-  end
-
-  # POST /artists
-  # POST /artists.json
-  def create
-    @artist = Artist.new(params[:artist])
-
-    respond_to do |format|
-      if @artist.save
-        format.html { redirect_to @artist, notice: 'Artist was successfully created.' }
-        format.json { render json: @artist, status: :created, location: @artist }
-      else
-        format.html { render action: "new" }
-        format.json { render json: @artist.errors, status: :unprocessable_entity }
-      end
-    end
-  end
-
-  # PUT /artists/1
-  # PUT /artists/1.json
-  def update
-    @artist = Artist.find(params[:id])
-
-    respond_to do |format|
-      if @artist.update_attributes(params[:artist])
-        format.html { redirect_to @artist, notice: 'Artist was successfully updated.' }
-        format.json { head :no_content }
-      else
-        format.html { render action: "edit" }
-        format.json { render json: @artist.errors, status: :unprocessable_entity }
-      end
-    end
-  end
-
-  # DELETE /artists/1
-  # DELETE /artists/1.json
-  def destroy
-    @artist = Artist.find(params[:id])
-    @artist.destroy
-
-    respond_to do |format|
-      format.html { redirect_to artists_url }
-      format.json { head :no_content }
-    end
-  end
+  # protected
+  # def collection
+  #   # TODO: Remove sort by user_id
+  #   @artists ||= end_of_association_chain.order("user_id desc").paginate(:page => params[:page])
+  # end
 end
